a390f578df4fed513edbb4f91fb7ce2c9767f144
xmerric <78622726+xmerric@users.noreply.github.com>
2021-02-06 22:08:03 +0000

    Update Michaels Installer.ps1

--- Michaels Installer.ps1
+++ Michaels Installer.ps1
@@ -1,28 +1,7 @@
 
 
 $wshell = New-Object -ComObject Wscript.Shell
-$Button = [System.Windows.MessageBoxButton]::YesNoCancel
-$ErrorIco = [System.Windows.MessageBoxImage]::Error
-$Ask = 'Do you want to run this as an Administrator?
-        Select "Yes" to Run as an Administrator
-        Select "No" to not run this as an Administrator
-        
-        Select "Cancel" to stop the script.'
 
-If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
-    $Prompt = [System.Windows.MessageBox]::Show($Ask, "Run as an Administrator or not?", $Button, $ErrorIco) 
-    Switch ($Prompt) {
-        #Installer running
-        Yes {
-            Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
-            Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
-            Exit
-        }
-        No {
-            Break
-        }
-    }
-}
 
 
 $Button1_Click = ({Write-Host "Installing Notepad++"
